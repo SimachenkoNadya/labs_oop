d050cf8dded86e326aa0b3f1e6ccc05d495bbe2f
Nadiia Simachenko <nadyasimachenko@gmail.com>
2024-12-16 04:57:53 +0200

    Added methods for defining elements and operations with them

--- java_lab_3/src/GildedRose.java
+++ java_lab_3/src/GildedRose.java
@@ -22,7 +22,7 @@ class Quality {
     }
 }
 
-class SellIn {
+class SellIn { //клас використовується для роботи зі значенням кількості днів до продажу товару
     private int value;
 
     public SellIn(int value) {
@@ -38,7 +38,74 @@ class SellIn {
     }
 }
 
-class GildedRose {
+class InventoryItem {
+    private final String name;
+    private final Quality quality;
+    private final SellIn sellIn;
+
+    public InventoryItem(String name, int sellIn, int quality) {
+        this.name = name;
+        this.quality = new Quality(quality);
+        this.sellIn = new SellIn(sellIn);
+    }
+
+    public void update() { //метод виконує оновлення стану конкретного елемента в інвентарі
+        if (isLegendary()) return;
+
+        updateQuality();
+        sellIn.decrease();
+
+        if (sellIn.getValue() < 0) {
+            handleExpired();
+        }
+    }
+
+    private boolean isLegendary() { //метод перевіряє, чи є об'єкт легендарним предметом
+        return name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private void updateQuality() { //Метод визначає, як змінювати якість предмета залежно від його типу
+        if (isAgedBrie()) {
+            quality.increase(1);
+        } else if (isBackstagePass()) {
+            updateBackstagePassQuality();
+        } else {
+            quality.decrease(1);
+        }
+    }
+
+    private void handleExpired() { //метод виконує спеціальну обробку для товарів, термін придатності яких закінчився
+        if (isAgedBrie()) {
+            quality.increase(1);
+        } else if (isBackstagePass()) {
+            quality.decrease(quality.getValue()); // Set to 0
+        } else {
+            quality.decrease(1);
+        }
+    }
+
+    private void updateBackstagePassQuality() { //Метод відповідає за оновлення якості квитків на концерт "Backstage passes"
+        quality.increase(1);
+        if (sellIn.getValue() < 10) {
+            quality.increase(1);
+        }
+        if (sellIn.getValue() < 5) {
+            quality.increase(1);
+        }
+    }
+
+    private boolean isAgedBrie() { //Метод визначає, чи є назва предмета "Aged Brie"
+        return name.equals("Aged Brie");
+    }
+
+    private boolean isBackstagePass() { //Метод перевіряє назву квитка
+        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+}
+
+
+
+    class GildedRose {
     Item[] items;
 
     public GildedRose(Item[] items) {
